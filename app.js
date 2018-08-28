010cf1f112beed52b2bb075715ebe6a19b1a6650
1000ch <shogo.sensui@gmail.com>
2018-08-29 00:24:16 +0900

    Fix demo script

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ import { createPatch } from './lib/patch.js';
 import { createVNodes } from './lib/vd.js';
 
 function render(vnode, rootNode) {
-  if (app.$vnode === undefined) {
-    app.$vnode = createVNodes(app.children);
+  if (rootNode.$vnode === undefined) {
+    rootNode.$vnode = createVNodes(rootNode.childNodes);
   }
 
-  const patch = createPatch(rootNode, app.$vnode, vnode);
-  app.$vnode = vnode;
+  const patch = createPatch(rootNode, rootNode.$vnode, vnode);
+  rootNode.$vnode = vnode;
   patch();
 }
 
 document.querySelector('button').addEventListener('click', () => {
+  const nextRoot = document.createDocumentFragment();
   const span = document.createElement('span');
   span.innerHTML = '＼(^o^)／';
-  const nextVnodes = createVNodes([span]);
-  render(nextVnodes, document.querySelector('#app'));
+  nextRoot.appendChild(span);
+
+  render(
+    createVNodes(nextRoot.childNodes),
+    document.querySelector('#app')
+  );
 });
